f778c1ceb2e7e25bd1a3aa3ea19359ff8d54bac0
Your Name <186149954+OliverDevsCode@users.noreply.github.com>
2025-01-20 14:31:56 +0000

    Yaw Rotation Working

--- src/camera.js
+++ src/camera.js
@@ -1,7 +1,10 @@
 let Camera = {
-  x:500,
-  y:500,
-  z:-100,
+  x:0,
+  y:0,
+  z:1000,
+  xYaw:0,
+  yYaw:0,
+  zYaw:0,
 }
 
 function drawCameraView(){
@@ -23,9 +26,29 @@ function drawCameraView(){
   depth = depth.sort((b, a) => a[1] - b[1]) 
   for(let i =0; i < depth.length;i++){
 
-    screenObjects[depth[i][0]].translate(Camera.x,Camera.y,Camera.z)
-    screenObjects[depth[i][0]].draw();
-    screenObjects[depth[i][0]].translate(-Camera.x,-Camera.y,-Camera.z)
+    let cameraCube = Object.assign(screenObjects[depth[i][0]])
+    console.log("Z BEFRORE ",cameraCube.vertex1.matrix[2])
+    cameraCube.translate(-Camera.x,-Camera.y,-Camera.z)
+    console.log("Z AFTER TRANS ",cameraCube.vertex1.matrix[2])
+    cameraCube.rotate("x",Camera.xYaw)
+    cameraCube.rotate("y",Camera.yYaw)
+    cameraCube.rotate("z",Camera.zYaw)
+    cameraCube.translate(500,500,0)
+    console.log("Z Before draw ",cameraCube.vertex1.matrix[2])
+    cameraCube.draw();
+    cameraCube.translate(-500,-500,0)
+    cameraCube.rotate("z",-Camera.zYaw)
+    cameraCube.rotate("y",-Camera.yYaw)
+    cameraCube.rotate("x",-Camera.xYaw)
+
+    cameraCube.translate(Camera.x,Camera.y,Camera.z)
+
+
+
+    // screenObjects[depth[i][0]].translate(Camera.x,Camera.y,Camera.z)
+    // // screenObjects[depth[i][0]].draw();
+    // screenObjects[depth[i][0]].translate(-Camera.x,-Camera.y,-Camera.z)
+
 
   }  
 
@@ -49,10 +72,14 @@ function mouseWheel(event){
   
 }
 
-function RotateObjects(valX,valY){
+function RotateObjects(valY,valX){
   for(let i =0; i < screenObjects.length;i++){
-    screenObjects[i].rotate("y",valX)
-    screenObjects[i].rotate("x",valY)
+    // screenObjects[i].rotate("y",valX)
+    // screenObjects[i].rotate("x",valY)
+
+    //TESTING CAMERA YAW PROPERTIES
+    // Camera.zYaw = -parseInt(valX)*0.5
+    // Camera.yYaw = -parseInt(valY)*0.5
 
   }
 }
@@ -66,18 +93,41 @@ function doubleClicked(){
 
 function CameraControls(){
   if (keyIsDown(LEFT_ARROW) === true) {
-    Camera.x -= 1;
+    Camera.x += 10;
   }
   
   if (keyIsDown(RIGHT_ARROW) === true) {
-    Camera.x += 1;
+    Camera.x -= 10;
   }
   
   if (keyIsDown(UP_ARROW) === true) {
-    Camera.y -= 1;
+    Camera.y -= 10;
   }
   
   if (keyIsDown(DOWN_ARROW) === true) {
-    Camera.y += 1;
+    Camera.y += 10;
+  }
+
+  //W +Y ROTATION
+  if (keyIsDown(87) === true) {
+    Camera.xYaw -= 1;
+  }
+
+  //S -Y ROTATION
+  if (keyIsDown(83) === true) {
+    Camera.xYaw += 1;
+  }
+  
+  //A -X ROTATION
+  if (keyIsDown(65) === true) {
+    Camera.yYaw += 1;
+    // Camera.zYaw += 1;
+
+  }
+
+  //D X ROTATION
+  if (keyIsDown(68) === true) {
+    Camera.yYaw -= 1;
+    // Camera.zYaw -= 1;
   }
 }
--- src/sketch.js
+++ src/sketch.js
@@ -1,6 +1,7 @@
 let cube;
 let cube2;
 let cube3;
+let zCube; //for testing z cord
 let rectangle;
 let xaxis;
 let yaxis;
@@ -21,10 +22,11 @@ function setup() {
   cube = new Cube(200,200,0,100)
   cube2 = new Cube(400,200,0,100)
   cube3 = new Cube(0,30,0,50)
+  zCube = new Cube(0,0,-200,25)
   yaxis = new Rectangle(0,10,0,2,2,500)
   zaxis = new Rectangle(0,10,0,500,2,2)
   xaxis = new Rectangle(0,10,0,2,500,2)
-  screenObjects.push(cube,cube2,cube3,xaxis,yaxis,zaxis); 
+  screenObjects.push(cube,cube2,cube3,xaxis,yaxis,zaxis,zCube); 
 
 }
 
@@ -42,7 +44,7 @@ function draw() {
   
   RotateObjects(joystick.valX,joystick.valY);
 
-  let fps = frameRate();
+  let fps = Math.floor(frameRate());
   push()
   fill(255,255,255)
   text(fps, 50, 50);
