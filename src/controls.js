2374edfe3ed19dead6a3d6c40e6558136832aa2d
OliverDevsCode <186149954+OliverDevsCode@users.noreply.github.com>
2025-01-25 14:28:49 +0000

    Pointer Locked Movement

--- src/controls.js
+++ src/controls.js
@@ -1,26 +1,9 @@
 function mouseWheel(event){
   if(event.delta < 0){
     Camera.fov -= 2.5;
-    
-
   }
   if(event.delta > 0){
     Camera.fov += 2.5;
-    
-
-
-  }
-}
-let prevX = 500;
-let prevY = 0;
-function mouseMoved(){
-  if(mouseY != prevY){
-    Camera.PITCH -= (prevY-mouseY)/10;
-    prevY = mouseY
-  }
-  if(mouseX != prevX){
-    Camera.YAW -= (prevX-mouseX)/10;
-    prevX = mouseX
   }
 }
 
@@ -49,12 +32,11 @@ function CameraControls(){
     Camera.z -= cos(Camera.YAW)*10
     Camera.x -= sin(Camera.ROLL)*10
     Camera.y -= sin(Camera.PITCH)*10
-   
+    
   }
 
   //S -Y ROTATION
   if (keyIsDown(83) === true) {
-    
     Camera.z += cos(Camera.YAW)*10
     Camera.x += sin(Camera.ROLL)*10
     Camera.y += sin(Camera.PITCH)*10
@@ -63,22 +45,45 @@ function CameraControls(){
   //A -X ROTATION
   if (keyIsDown(65) === true) {
     Camera.x -= 10;
-
-
   }
 
   //D X ROTATION
   if (keyIsDown(68) === true) {
     Camera.x += 10;
-
   }
 
   if (keyIsDown(81) === true) {
     Camera.y -= 10;
-
   }
   if (keyIsDown(69) === true) {
     Camera.y += 10;
-
   }
 }
+
+function doubleClicked() {
+  if (!isPointerLocked) {
+    canvas.requestPointerLock();
+  }
+}
+
+// Event listener for pointer lock change
+function onPointerLockChange() {
+  isPointerLocked = document.pointerLockElement === canvas;
+  if (!isPointerLocked) {
+    console.log("Pointer unlocked!");
+  }
+}
+
+// Event listener for mouse movement
+function onMouseMove(event) {
+  if (isPointerLocked) {
+    // Update deltas
+    deltaX = event.movementX;
+    deltaY = event.movementY;
+    console.log(`X:${deltaX}, Y:${deltaY}`)
+    Camera.PITCH += (deltaY)/10;
+    Camera.YAW += (deltaX)/10;
+  
+  }
+}
+
--- src/sketch.js
+++ src/sketch.js
@@ -1,14 +1,21 @@
-
+let isPointerLocked = false;
+let deltaX = 0;
+let deltaY = 0;
 
 function setup() {
   createCanvas(1000, 1000);
+
   // createBasicScene()
   createGameDemo()
+
+  // Add event listeners for pointer lock
+  document.addEventListener('pointerlockchange', onPointerLockChange);
+  document.addEventListener('mousemove', onMouseMove);
   
 }
 
 function draw() {
-  // frameRate(100) //Change for smoother experience
+  // frameRate(160) //Change for smoother experience
   angleMode(DEGREES)
   background(100);
   drawCameraView();
